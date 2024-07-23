eb1f435590eda699fa175a1d84393cd9ca3dee06
Beth McKinney <143623621+macbetthh@users.noreply.github.com>
2024-07-22 20:54:53 -0600

    copied error page from a previous activity - added pages folder to src folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css';
+
+//pages the router will use
+import App from './App.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <aboutme />,
+      },
+      {
+        path: 'profile/:id',
+        element: <ProfilePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
